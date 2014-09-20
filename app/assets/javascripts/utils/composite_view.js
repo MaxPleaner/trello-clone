885287120a70a615fe7632a27fee6def64e0711f
App Academy Student <student@appacademy.io>
2014-09-19 18:06:03 -0700

    closing for day

--- app/assets/javascripts/utils/composite_view.js
+++ app/assets/javascripts/utils/composite_view.js
@@ -73,5 +73,6 @@ Backbone.CompositeView = Backbone.View.extend({
       this._subviews[selector] = this._subviews[selector] || [];
       return this._subviews[selector];
     }
-  }
+  },
+  
 });
--- app/assets/javascripts/views/lists_new.js
+++ app/assets/javascripts/views/lists_new.js
@@ -1,6 +1,8 @@
 TrelloClone.Views.ListsNew = Backbone.View.extend({
     template: JST['lists/new'],
 
+    tagName: "section",
+
     events: {
       "submit form": "submitList"
     },
@@ -17,16 +19,19 @@ TrelloClone.Views.ListsNew = Backbone.View.extend({
       event.preventDefault();
       var params = $(event.currentTarget).serializeJSON();
       var newList = new TrelloClone.Models.List(params['list'])
-      newList.set({board_id: this.model.id })
-      var that = this;
-      newList.save({}, {
-        success: function () {
-          TrelloClone.Collections.lists.add(newList);
-          that.model.fetch();
-           Backbone.history.navigate("boards/" + that.model.id, { trigger: true });
-        }
-      });
-            
+      if (params.list['title'].length === 0) {
+       this.$('form').prepend("Must add a title"); 
+      } else {
+        newList.set({board_id: this.model.id })
+        var that = this;
+        newList.save({}, {
+          success: function () {
+            TrelloClone.Collections.lists.add(newList);
+            that.model.fetch();
+             Backbone.history.navigate("boards/" + that.model.id, { trigger: true });
+          }
+        });     
+      }
     },
           
 });
